558ea79b0edf021754012aac11f916c52e519597
Sahil Bhimjiani <46335522+sahil9701@users.noreply.github.com>
2021-05-03 11:14:43 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          asdf
+          max_age 500 seconds
         </p>
         <a
           className="App-link"
