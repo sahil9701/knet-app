7237f36c1fbc1172ca2ee4c06aff8f6114129a4e
Sahil Bhimjiani <46335522+sahil9701@users.noreply.github.com>
2021-05-03 10:45:23 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test changes to see cache behavior. You rock!!
+          cache behavior. You rock!!
         </p>
         <a
           className="App-link"
