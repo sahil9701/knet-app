117e269864b8f324e9348c7b27b105a20fba722e
Sahil Bhimjiani <46335522+sahil9701@users.noreply.github.com>
2021-05-03 11:30:30 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          s-maxage 500 seconds
+          maxage 0 seconds
         </p>
         <a
           className="App-link"
